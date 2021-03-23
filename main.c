e467d0d9574e603735a21b33ecbbfb4ed9d2b749
Henrique Krauss <henrique.krauss22@gmail.com>
2021-03-23 13:41:34 -0300

    Changing input scanf to int instead of char

--- main.c
+++ main.c
@@ -17,28 +17,26 @@ double * getNumbers() {
 
 int main() {
     while (1) {
-        char input[1], result[20];
-        int operation;
+        int input;
         double *bothNumbers, num1, num2;
-        printf("\n\n1 - Addition \n2 - Subtraction \n3 - Multiplication \n4 - Division \n5 - Quit \nChoose an operation: ");
-        scanf("%s", input);
-        operation = atoi(input);
+        printf("\n\n1 - Addition \n2 - Subtraction \n3 - Multiplication \n4 - Division \n5 - Quit \nChoose an input: ");
+        scanf("%d", &input);
 
-        if (operation < 5 && operation > 0) {
+        if (input < 5 && input > 0) {
             bothNumbers = getNumbers();
             num1 = bothNumbers[0];
             num2 = bothNumbers[1];
         }
 
-        if (operation == 1) {
+        if (input == 1) {
             printf("The addition of %.2f and %.2f is: %.2f", num1, num2, add(num1, num2));      
-        } else if (operation == 2) {
+        } else if (input == 2) {
             printf("The subtraction of %.2f and %.2f is: %.2f", num1, num2, subtract(num1, num2));
-        } else if (operation == 3) {
+        } else if (input == 3) {
             printf("The multiplication of %.2f and %.2f is: %.2f", num1, num2, multiply(num1, num2));
-        } else if (operation == 4) {
+        } else if (input == 4) {
             printf("The division of %.2f and %.2f is: %.2f", num1, num2, divide(num1, num2));
-        } else if (operation == 5) {
+        } else if (input == 5) {
             break;
         }
     }
